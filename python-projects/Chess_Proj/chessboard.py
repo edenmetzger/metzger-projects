decaee728fb4435d3fd92cd62dfcd5226554fee1
Eden Metzger <edenmetzger@Edens-MacBook-Pro.local>
2019-10-25 17:46:45 -0400

    chess board with notation

--- python-projects/Chess_Proj/chessboard.py
+++ python-projects/Chess_Proj/chessboard.py
@@ -5,27 +5,41 @@ import stdio
 run = True
 white = (255, 255, 255)
 black = (0, 0, 0)
+blue = (0, 0, 128)
 
 pygame.init()
 window = pygame.display.set_mode((900,800))
 pygame.display.set_caption("Chessboard")
 
-boardLength = 8
-size = 80
+dimensions_of_board = 8
+square = 80
 window.fill(white)
 
-x = 0
-for i in range(1, boardLength + 1):
-    for j in range(1, boardLength + 1):
-        if x % 2 == 0:
-            pygame.draw.rect(window, white,[size*j, size*i, size, size])
-        else:
-            pygame.draw.rect(window, black, [size*j, size*i, size, size])
-        x+=1
-    x-=1
-pygame.draw.rect(window, black, [size, size, boardLength*size, boardLength*size], 1)
+spot = 0
+for i in range(1, dimensions_of_board + 1):
+	for j in range(1, dimensions_of_board + 1):
+		if spot % 2 == 0:
+			pygame.draw.rect(window, white, [square * j, square * i, square, square])
+		else:
+			pygame.draw.rect(window, black, [square * j, square * i, square, square])
+		spot += 1
+	spot -= 1
+pygame.draw.rect(window, black, [square, square, dimensions_of_board*square, dimensions_of_board*square],  1)
 
 pygame.display.update()
+
+font = pygame.font.Font('freesansbold.ttf', 45)
+text = font.render('A    B    C    D    E    F    G    H', True, blue, white)
+window.blit(text, [93, 30])
+
+font2 = pygame.font.Font('freesansbold.ttf', 50)
+text2 = font2.render('1    2    3    4    5    6    7    8', True, blue, white)
+text2 = pygame.transform.rotate(text2, 90)
+window.blit(text2, [20, 95])
+
+
+pygame.display.update()
+
 
 while run:
 	for event in pygame.event.get():
